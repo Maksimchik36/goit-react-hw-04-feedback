89b3f422f9d19ef508d9869df9b1cee88d559a5a
Maksimchik36 <Peltek1985@gmail.com>
2022-07-07 16:50:48 +0300

    commit statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import Statistics from "./Statistics";
+// import Statistics from "./Statistics";
 
 class App extends Component {
   static defaultProps = {};
